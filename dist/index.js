e59ba2aec1e623ab9c995c948e379881ad51fc26
Muhib656 <160688677+Muhib656@users.noreply.github.com>
2024-03-30 07:56:25 +0500

    My First Commit
    This is a simple number choosing game

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+#! /urs/bin/env node 
+import inquirer from "inquirer";
+// Choose a Random Number
+// User's guess a number
+// Compare Users number with computer choosen random number and print final answer
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+        name: "usersGuessNumber",
+        type: "number",
+        message: "Please guess a right number between 1 to 10:"
+    }
+]);
+if (answer.usersGuessNumber === randomNumber) {
+    console.log("Congragulations! You Won.");
+}
+else {
+    console.log("Try again, You guessed a wrong number.");
+}

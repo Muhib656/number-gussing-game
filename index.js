22ce2bbcca65b1af5aa3b5c363dcbceebd2fcb0a
Muhib Ahmed <a.muhib656@gmail.com>
2024-04-16 08:32:24 +0500

    Adding chalk

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node 
 import inquirer from "inquirer";
+import chalk from "chalk";
 // Choose a Random Number
 // User's guess a number
 // Compare Users number with computer choosen random number and print final answer
@@ -7,12 +8,12 @@ const randomNumber = Math.floor(Math.random() * 10 + 1);
 const answer = await inquirer.prompt([{
         name: "usersGuessNumber",
         type: "number",
-        message: "Please guess a right number between 1 to 10:"
+        message: chalk.yellow("Please guess a right number between 1 to 10:")
     }
 ]);
 if (answer.usersGuessNumber === randomNumber) {
-    console.log("Congragulations! You Won.");
+    console.log(chalk.green("Congragulations! You Won."));
 }
 else {
-    console.log("Try again, You guessed a wrong number.");
+    console.log(chalk.redBright("Try again, You guessed a wrong number."));
 }
